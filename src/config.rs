ca4331422c36a9d52ed824160ca28ff7d132ac9e
liudewensss-code <liudewensss@gmail.com>
2025-11-11 21:25:44 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -105,8 +105,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["114.66.52.77"];
+pub const RS_PUB_KEY: &str = "ghJZKKFyS+xGvxv3Yb8TMLXA9AmQ4M7RspIZUSmgAVY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
